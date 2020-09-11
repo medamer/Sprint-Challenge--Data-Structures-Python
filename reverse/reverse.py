52791ecc773e7eee627137c1171cb32ff6cff8de
Mohamed Edamer <edamer.mo@gmail.com>
2020-09-11 10:49:01 -0700

    missing Ringbuffer

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,12 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        prev = None
+        node = self.head
+        while node:
+            nxt = node.next_node
+            node.next_node = prev
+            prev = node
+            node = nxt
+        self.head = prev
+
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,32 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.queue = []
+        self.size = len(self.queue)
+        self.next = None
+        self.head = None
+        self.tail = None
 
     def append(self, item):
-        pass
+        if self.head is None:
+            self.queue.append(item)
+        elif self.size < self.capacity:
+            item
+        else:
+            self.tail = (self.tail + 1) % self.capacity
+            self.queue[self.tail] = item
 
     def get(self):
-        pass
+        if self.head == -1:
+            print([])
+        elif self.tail >= self.head:
+            for i in range(self.head, self.tail + 1):
+                print(self.queue[i])
+            print()
+        else:
+            for i in range(self.head, self.capacity):
+                print(self.queue[i])
+            for i in range(0, self.tail + 1):
+                print(self.queue[i])
+            print()
+            
